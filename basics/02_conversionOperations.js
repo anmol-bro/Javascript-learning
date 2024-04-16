fea7c2086d856f2169118108236f2b8c661eb04d
anmol-bro <76219206+anmol-bro@users.noreply.github.com>
2024-04-16 20:45:56 +0000

    conversion of datatypes

---
+++ basics/02_conversionOperations.js
@@ -0,0 +1,18 @@
+let Score = "45"
+let userName = "45Rohit"
+
+
+let ScoreInNumber = Number(Score)
+let userNameInNumber = Number(userName)
+
+
+console.log(ScoreInNumber)
+console.log(typeof(ScoreInNumber))
+
+console.log(userNameInNumber)
+console.log(typeof(userNameInNumber))
+
+//"33"=> 33
+//"33abc" => NaN
+//null => 0
+//true => 1
