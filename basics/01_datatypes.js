ad215610f1fce8d69a5970737e99f26e841a3107
anmol-bro <76219206+anmol-bro@users.noreply.github.com>
2024-04-16 20:20:24 +0000

    datatypes

---
+++ basics/01_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3)// we are using node js not browser
+//code readlibality should be high
+//TC39 - ECMA and mdn for documentation
+
+
+//number => 2 to power 53
+//bigInt
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined => variable decalred but not assigned
+//symbol=> uniqueness
+
+
+//object
+
+console.log(typeof "Anmol")
+console.log(typeof 36)
+console.log(typeof null) //special case
+console.log(typeof undefined) //special case
