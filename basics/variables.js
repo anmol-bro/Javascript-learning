d09bd84a16eefc193bcfaf691e7e7712ee1259b1
anmol-bro <76219206+anmol-bro@users.noreply.github.com>
2024-04-16 19:55:57 +0000

    basic variable interaction

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accounId = 14454  // value fixed 
+let accountName = "Anmol Kumar" // prefferable - value is variable within a scope 
+var accountPass = "12345" //value variable globally
+accountCity = "Gorakhpur" // not a good way
+let accountState
+
+/*
+accounId = 2 not allowed
+*/
+accountName = "Anmol Pandey"
+accountPass = "98765" 
+accountCity = "Noida"
+
+
+
+console.table([accounId,accountName,accountPass,accountCity,accountState])
