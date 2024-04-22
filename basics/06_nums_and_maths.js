b0c65b7926e96eb829ca9762c973d1c6bb1405ee
anmol-bro <76219206+anmol-bro@users.noreply.github.com>
2024-04-22 19:45:19 +0000

    numbers and maths

---
+++ basics/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(5000)
+//console.log(balance)
+
+console.log(balance.toString().length)
+//console.log(balance.toFixed(2)) 
+
+const otherNumber = 23.8965
+//console.log(otherNumber.toPrecision(2)) // gives the precise value of a number
+//console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN')); // by default in american standards 'en-In' for indian number system
+
+// ++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++
+/*
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6)) ;
+console.log(Math.ceil(4.2));
+console.log(Math.ceil(4.9));
+console.log(Math.ceil(4,9,3,7));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+*/
+
+const min = 10
+const max = 20
+
+console.log(Math.floor (Math.random() * (max -min + 1)) + min); // formula for finading a number between two values
+
+
+
+
+
