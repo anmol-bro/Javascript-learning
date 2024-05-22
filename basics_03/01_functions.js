0552c7cf16f8cde7dafdc64670e58483f2f00540
anmol-bro <76219206+anmol-bro@users.noreply.github.com>
2024-05-22 12:20:26 +0000

    function

---
+++ basics_03/01_functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+console.log("A");
+console.log("N");
+console.log("M");
+console.log("o");
+console.log("L");
+}
+
+//sayMyName()
+
+function addTwoNumber(a,b){
+    console.log(a+b);
+}
+addTwoNumber(5,6)
