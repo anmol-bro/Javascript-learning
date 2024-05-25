a8204ae19018b4c7117392d7ad138d59471893cd
anmol-bro <76219206+anmol-bro@users.noreply.github.com>
2024-05-25 12:14:00 +0000

    functions done

--- basics_03/01_functions.js
+++ basics_03/01_functions.js
@@ -8,7 +8,48 @@ console.log("L");
 
 //sayMyName()
 
-function addTwoNumber(a,b){
-    console.log(a+b);
+function addTwoNumber(a,b){            //a,b called parameters
+    //console.log(a+b);
+    return a+b;                        //return is last executable code inside the function
 }
-addTwoNumber(5,6)
+addTwoNumber(5,6)                    //a,b called arguments
+ // either you store the function call or print on console
+
+
+ function userLoggedIn(userName){
+    if(!userName){
+            console.log('Enter the username')
+            return
+    }
+    return `${userName} , just logged in`
+ }
+
+ console.log(userLoggedIn("Anmol"))
+
+
+ function calculateCartPrice(...num1){              // ... is known as rest or spread operater on the basis of their usecases
+      return num1;
+ }
+
+ console.log(calculateCartPrice(200,500,1000))    // returns array of values ... using loop you can add the elements
+
+
+
+ const user = {
+    username : "Anmol",
+    Price : 199
+ }
+
+ function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.Price}`)
+ }
+
+ handleObject(user)
+
+
+ const myNewArray = [100,200,500,400]
+
+ function secondelement(array){
+    console.log(`second element of the array is ${array[1]}`)
+ }
+ secondelement(myNewArray)
