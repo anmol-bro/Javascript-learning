d8a429da6e9ec80270d545d1c5738a8b8522827d
anmol-bro <76219206+anmol-bro@users.noreply.github.com>
2024-07-24 21:20:29 +0000

    filter on object array

--- iterations/06_forEach2.js
+++ iterations/06_forEach2.js
@@ -14,12 +14,54 @@ myNums.filter( (num)=> num > 5 );
 
 const newNums = myNums.filter( (num)=> num > 5 )    // remember this that it is a explicit way of arrow function
 
-console.log(newNums)
+//console.log(newNums)
 
 // const newNums2 = myNums.filter( (num)=>{
 //     return num >5                                  //same as before but  here we must write return keyword as we have opened the scope - implicite
 // } )
 
+
+const books = [
+    {
+        title: "Book One",
+        genre: "History",
+        publish: 1996,
+        edition : 2008
+    },
+    {
+        title: "Book Three",
+        genre: "Science",
+        publish: 1990,
+        edition : 2005
+    },
+    {
+        title: "Book Four",
+        genre: "History",
+        publish: 2018,
+        edition : 2023
+    },
+    {
+        title: "Book Five",
+        genre: "History",
+        publish: 1956,
+        edition : 2014
+    },
+    {
+        title: "Book Six",
+        genre: "History",
+        publish: 1987,
+        edition : 1998
+    },
+    {
+        title: "Book Seven",
+        genre: "History",
+        publish: 2001,
+        edition : 2008
+    }
+]
+
+//console.log(books.filter( (bk)=>(bk.publish> 1990) )); // can iterate on object arrays
+//console.log(books.filter( (bk)=>(bk.publish> 2000 && bk.genre=="History") ));
 
 
 
